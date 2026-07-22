e875568b382aa0fec2aa295b9c50715b46beaf5c
avell37 <shrokov.37@gmail.com>
2026-07-22 06:09:23 +0300

    fix: rewrite docker-entrypoint

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 set -e
 
-echo "--- Running migrations"
-npx prisma migrate deploy --config prisma.config.ts
-
-echo "--- Running seed"
-npx prisma db seed --config prisma.config.ts
-
 echo "--- Starting server"
 node dist/src/main.js
